da0e0cdc734eb776cdece3d9cc4a25ef3c7da813
Robert Fono <fonorobert@gmail.com>
2025-02-10 16:25:14 +0100

    move to landscape-based layout

--- zinegen.sh
+++ zinegen.sh
@@ -3,14 +3,16 @@
 # set defaults
 
 BASE_DIR=.
+PORTRAIT=false
 
 
 # get options
 
-while getopts d: flag
+while getopts d:p flag
 do
     case "${flag}" in
 		d) BASE_DIR=${OPTARG};;
+		p) PORTRAIT=true;;
         # o) DIR_OUT=${OPTARG};;
         # s) CSS_PATH=${OPTARG};;
     esac
@@ -19,6 +21,9 @@ done
 DIR_OUT=$BASE_DIR/build
 CSS_PATH=$BASE_DIR/custom.css
 
+printf '\n'
+printf 'Generating zine into %s with %s as stylesheet. Portrait mode is %s.' $DIR_OUT $CSS_PATH $PORTRAIT
+printf '\n\n'
 
 # remove flags so arg list is only input files
 shift $((OPTIND - 1))
@@ -42,8 +47,12 @@ rm -f $DIR_OUT/output.pdf
 ARGS=("$@")
 FILES=${ARGS[@]}
 
+printf 'Rendering pages...'
+printf '\n'
+
 for FILE in $FILES
 do
+
 
 	FILENAME=$(basename -- "$FILE")
 	FILENAME="${FILENAME%.*}"
@@ -52,12 +61,41 @@ do
 		-s -f markdown --template="$BASE_DIR/template.html" --pdf-engine=weasyprint \
 		-c $CSS_PATH
 
+	printf '"%s"   ' "$FILENAME"
+	printf '\n'
 done
 
+printf '\n'
+
+printf 'Joining pages...'
+printf '\n'
+
 psjoin $DIR_OUT/temp/*.pdf > $DIR_OUT/temp/joined.pdf
-qpdf $DIR_OUT/temp/joined.pdf --pages . 4,3,2,1,5-8 -- $DIR_OUT/temp/reordered.pdf
-qpdf --rotate=+180:1-4 $DIR_OUT/temp/reordered.pdf $DIR_OUT/temp/flipped.pdf --flatten-rotation
-psnup -p A4 -8 $DIR_OUT/temp/flipped.pdf $DIR_OUT/output.pdf
+
+if [ $PORTRAIT == false ]; then
+	# qpdf $DIR_OUT/temp/joined.pdf --pages . 4,3,2,1,5-8 -- $DIR_OUT/temp/reordered.pdf
+	qpdf --rotate=+180:1 $DIR_OUT/temp/joined.pdf $DIR_OUT/temp/flipped.pdf --flatten-rotation
+
+	printf 'Generating printable PDF...'
+	printf '\n\n'
+
+	psnup -p "297mmx210mm" -c -8 $DIR_OUT/temp/flipped.pdf $DIR_OUT/output.pdf
+	# qpdf --rotate=-180:1 $DIR_OUT/output.pdf --replace-input --flatten-rotation
+else
+	qpdf $DIR_OUT/temp/joined.pdf --pages . 4,3,2,1,5-8 -- $DIR_OUT/temp/reordered.pdf
+	qpdf --rotate=+180:1 $DIR_OUT/temp/joined.pdf $DIR_OUT/temp/flipped.pdf --flatten-rotation
+
+	printf 'Generating printable PDF...'
+	printf '\n\n'
+
+	psnup -p "210mmx297mm" -c -8 $DIR_OUT/temp/flipped.pdf $DIR_OUT/output.pdf
+	qpdf --rotate=-90:1 $DIR_OUT/output.pdf --replace-input --flatten-rotation
+
+fi
 
 # clean up build artifacts
 rm -r $DIR_OUT/temp
+
+printf '\n'
+printf 'Zine generation complete!'
+printf '\n'
